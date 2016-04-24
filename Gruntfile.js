9d5ce237fb81314ad4faf25d079872d203a79ae9
Anderson Menezes <andersomxp@gmail.com>
2016-04-24 14:46:43 -0300

    Compliled sass and new path

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
 		sass: {
 			dev: {
 				files: {
-					'assets/css/main.css': 'source/sass/_main.sass'
+					'assets/css/main.css': 'source/sass/main.sass'
 				}
 			}
 		},
